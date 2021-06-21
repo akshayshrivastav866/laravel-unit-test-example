72a3388ac28504cc782e58ef5dcf34698b9277b5
sys_cracker <akshayshrivastav866@gmail.com>
2021-06-21 09:46:16 +0530

    menucontroller unit test passed

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -87,60 +87,61 @@ class EventsController extends BaseController
 						];
 		}
 
-	public function getFutureEventsWithWorkshops() {
-		return 	[
-						[
-							 "id" => 2, 
-							 "name" => "Laravel convention 2021", 
-							 "created_at" => "2021-04-20T07:01:14.000000Z", 
-							 "updated_at" => "2021-04-20T07:01:14.000000Z", 
-							 "workshops" => [
-								[
-								   "id" => 2, 
-								   "start" => "2021-10-21 10:00:00", 
-								   "end" => "2021-10-21 18:00:00", 
-								   "event_id" => 2, 
-								   "name" => "The new Eloquent - load more with less", 
-								   "created_at" => "2021-04-20T07:01:14.000000Z", 
-								   "updated_at" => "2021-04-20T07:01:14.000000Z" 
-								], 
-								[
-									  "id" => 3, 
-									  "start" => "2021-11-21 09:00:00", 
-									  "end" => "2021-11-21 17:00:00", 
-									  "event_id" => 2, 
-									  "name" => "AutoEx - handles exceptions 100% automatic", 
-									  "created_at" => "2021-04-20T07:01:14.000000Z", 
-									  "updated_at" => "2021-04-20T07:01:14.000000Z" 
-								   ] 
-							 ] 
-						], 
-						[
-							 "id" => 3, 
-							 "name" => "React convention 2021", 
-							 "created_at" => "2021-04-20T07:01:14.000000Z", 
-							 "updated_at" => "2021-04-20T07:01:14.000000Z", 
-							 "workshops" => [
-								[
-								   "id" => 4, 
-								   "start" => "2021-08-21 10:00:00", 
-								   "end" => "2021-08-21 18:00:00", 
-								   "event_id" => 3, 
-								   "name" => "#NoClass pure functional programming", 
-								   "created_at" => "2021-04-20T07:01:14.000000Z", 
-								   "updated_at" => "2021-04-20T07:01:14.000000Z" 
-								], 
-								[
-									  "id" => 5, 
-									  "start" => "2021-08-21 09:00:00", 
-									  "end" => "2021-08-21 17:00:00", 
-									  "event_id" => 3, 
-									  "name" => "Navigating the function jungle", 
-									  "created_at" => "2021-04-20T07:01:14.000000Z", 
-									  "updated_at" => "2021-04-20T07:01:14.000000Z" 
-								   ] 
-							 ] 
-						]
-					];
-	}
+	public function getFutureEventsWithWorkshops()
+		{
+			return 	[
+							[
+								 "id" => 2, 
+								 "name" => "Laravel convention 2021", 
+								 "created_at" => "2021-04-20T07:01:14.000000Z", 
+								 "updated_at" => "2021-04-20T07:01:14.000000Z", 
+								 "workshops" => [
+									[
+									   "id" => 2, 
+									   "start" => "2021-10-21 10:00:00", 
+									   "end" => "2021-10-21 18:00:00", 
+									   "event_id" => 2, 
+									   "name" => "The new Eloquent - load more with less", 
+									   "created_at" => "2021-04-20T07:01:14.000000Z", 
+									   "updated_at" => "2021-04-20T07:01:14.000000Z" 
+									], 
+									[
+										  "id" => 3, 
+										  "start" => "2021-11-21 09:00:00", 
+										  "end" => "2021-11-21 17:00:00", 
+										  "event_id" => 2, 
+										  "name" => "AutoEx - handles exceptions 100% automatic", 
+										  "created_at" => "2021-04-20T07:01:14.000000Z", 
+										  "updated_at" => "2021-04-20T07:01:14.000000Z" 
+									   ] 
+								 ] 
+							], 
+							[
+								 "id" => 3, 
+								 "name" => "React convention 2021", 
+								 "created_at" => "2021-04-20T07:01:14.000000Z", 
+								 "updated_at" => "2021-04-20T07:01:14.000000Z", 
+								 "workshops" => [
+									[
+									   "id" => 4, 
+									   "start" => "2021-08-21 10:00:00", 
+									   "end" => "2021-08-21 18:00:00", 
+									   "event_id" => 3, 
+									   "name" => "#NoClass pure functional programming", 
+									   "created_at" => "2021-04-20T07:01:14.000000Z", 
+									   "updated_at" => "2021-04-20T07:01:14.000000Z" 
+									], 
+									[
+										  "id" => 5, 
+										  "start" => "2021-08-21 09:00:00", 
+										  "end" => "2021-08-21 17:00:00", 
+										  "event_id" => 3, 
+										  "name" => "Navigating the function jungle", 
+										  "created_at" => "2021-04-20T07:01:14.000000Z", 
+										  "updated_at" => "2021-04-20T07:01:14.000000Z" 
+									   ] 
+								 ] 
+							]
+						];
+		}
 }
